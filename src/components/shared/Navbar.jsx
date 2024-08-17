2f3795304e2247b5935bfcb37e8e03a964541545
Pranto Deb <rajprit0m@outlook.com>
2024-08-17 21:08:45 +0600

    menu itom click to open section and hide menu bar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,10 +1,11 @@
-// src/components/Navbar.js
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "../../utils/classNames";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
@@ -12,19 +13,18 @@ const Navbar = () => {
     useEffect(() => {
         document.documentElement.classList.add("dark");
     }, []);
+
+    const handleTo = (to) => {
+        navigate(to)
+        setMenuOpen(!menuOpen);
+    }
     return (
         <div className="relative">
             <nav className="max-w-[1550px] w-[90%] mx-auto fixed top-0 left-0 right-0 bg-transparent text-white p-4 flex items-center justify-between z-50">
                 <div className="text-lg font-bold">PD.</div>
                 <div className="flex items-center">
-                    <button
-                        onClick={toggleMenu}
-                        className="bg-white text-purple-900 px-4 py-2 rounded-lg"
-                    >
+                    <button onClick={toggleMenu} className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-3xl font-bold transform hover:-translate-y-1 transition duration-400">
                         Menu
-                    </button>
-                    <button className="shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-3xl font-bold transform hover:-translate-y-1 transition duration-400">
-                        Figma Outline
                     </button>
 
                 </div>
@@ -39,18 +39,18 @@ const Navbar = () => {
                     { hidden: !menuOpen }
                 )}
             >
-                <a href="#home" className="p-4">
+                <Link onClick={() => handleTo("#home")} className="p-4">
                     Home
-                </a>
-                <a href="#projects" className="p-4">
+                </Link>
+                <Link onClick={() => handleTo("#projects")} className="p-4">
                     Projects
-                </a>
-                <a href="#about" className="p-4">
+                </Link>
+                <Link onClick={() => handleTo("#about")} className="p-4">
                     About
-                </a>
-                <a href="#contact" className="p-4">
+                </Link>
+                <Link onClick={() => handleTo("#contact")} className="p-4">
                     Contact
-                </a>
+                </Link>
             </motion.div>
         </div>
     );
